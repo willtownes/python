1f1aafd6c510ec58b188a3c56d91b69ede929796
Will Townes <will.townes@gmail.com>
2011-06-02 16:09:00 -0400

    stub file to make an are you a hater program (iphone app?)

---
+++ haters.py
@@ -0,0 +1,35 @@
+#Program to determine if you are a player hater and if so what kind
+
+''' Notes on Haters:
+nice car
+wife
+money
+good looking
+
+Haters:
+Ice T - host of awards show. Hating is an artform, born that way. Dislikes:
+    - Bill Cosby on crack
+
+Buc Nasty - blue suit bombed out and depleted like Afghanistan
+ - got his best friend's wife pregnant
+
+Beautiful- little richard curls, ugliest hater
+- broke ass sexual chocolate
+
+Star - Celebrity Hater. Hat and beard
+
+Mr. Roboto - top player hater from Korea
+
+Silky Johnson - cane, fur coat
+ - called in a bomb threat to the special olympics
+
+Pit Bull - accuses Ice T of being corny
+ - called police on the drug dealers next door b/c he was jealous of the money they were making
+ - jealous of other haters
+
+hating on:
+Puff Daddy - shiny dolphin teeth
+Osama bin Laden - pumping gas
+Ozzy Osbourne's daughter - daughter don't sing
+Rosie Odonnell - Boy George
+'''
