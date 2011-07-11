a612391c93bfd300d46c921bc29b7ab32e0f86f6
Will Townes <will.townes@gmail.com>
2011-07-11 11:29:29 -0400

    updated to use csv module, it now escapes dollar signs and (where quoted) commas

--- csv2latex.py
+++ csv2latex.py
@@ -1,14 +1,23 @@
 #Program to convert CSV formatted tables into LaTeX format
-#By Will Townes
-import os
+#By Will Townes, will.townes@gmail.com
+#July 2011
+import os, csv
 path = os.environ['USERPROFILE']+'\\Desktop'
 ifile = '\\data.csv'
 ofile = '\\LaTeXdata.txt'
-f = open(path+ifile,'r').readlines()
-g = open(path+ofile,'w')
-for i in range(len(f)):
-    f[i] = f[i].replace(',','&')
-    f[i] = f[i].replace('\n',r'\\'+'\n')
-    g.write(f[i])
-g.close()
+f = csv.DictReader(open(path+ifile,'rb'))
+ofile2 = open(path+ofile,'wb')
+csv.register_dialect('latex',delimiter='&',quotechar='"') #define LaTeX dialect
+g = csv.DictWriter(ofile2,f.fieldnames,dialect='latex',extrasaction='raise')
+g.writerow(dict((h,h) for h in f.fieldnames)) #write header row
+for i in f:
+    g.writerow(i)
+ofile2.close()
+
+x = open(path+ofile,'r').readlines()
+y = open(path+ofile,'w')
+for i in x: #escape all dollar signs for currency data, add '\\' to end of lines
+    y.write(i.replace('$','\\$').replace('\n','\\\\\n'))
+y.close()
+    
 
