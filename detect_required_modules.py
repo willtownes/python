e097c097d6a9b20753204e99582b5ce9bdb434c9
Will Townes <will.townes@gmail.com>
2012-02-06 16:19:08 -0500

    script to list out the modules used by other scripts in a given directory

--- detect_required_modules.py
+++ detect_required_modules.py
@@ -5,7 +5,7 @@ Will Townes
 
 import os, re
 #path = raw_input('Please paste in the directory you want to inspect')
-path = r"C:\Documents and Settings\townesw\My Documents\work\scripts"
+path = os.path.join(os.environ['USERPROFILE'],r'My Documents\work\scripts')
 q = os.walk(path)
 files = []
 for root,directory,fil in os.walk(path):
@@ -18,4 +18,26 @@ for j in files:
 modules = []
 for i in pys:
     f = open(i,'r').readlines()
-    modules.extend([line for line in f if 'import' in line])
+    #include all "import" statements except those that are commented out.
+    modules.extend([line.rstrip() for line in f if 'import' in line and line[0] != '\#'])
+modules.sort()
+modules = list(set(modules)) #remove duplicates
+for i in range(len(modules)):
+    if '#' not in modules[i]:
+        pass
+    else:
+        modules[i] = modules[i].split('#')[0] #get rid of everything to the right of the comment marker
+modules = [i for i in modules if 'import' in i] #get rid of lines without import statements
+pkg = set()
+for i in modules:
+    if 'from' in i:
+        pkg.add(i.split(' ')[1])
+    elif 'as' in i:
+        pkg.add(i.split('as')[0].split(' ')[1])
+    else:
+        j = i.split('import')[1].split(',')
+        j = set([k.strip() for k in j])
+        pkg = pkg|j #set union operation adds the additional packages to the list
+print('The following modules are needed to run the scripts:')
+for i in sorted(list(pkg)):
+    print(i)
