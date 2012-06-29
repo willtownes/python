aaad6ea1835fdac03b7b3909fd690e4070e7b154
Will Townes <will.townes@gmail.com>
2012-06-29 18:08:28 -0400

    minor changes to python web practice files

--- LearnPythonHardWay/projects/gothonweb/bin/app.py
+++ LearnPythonHardWay/projects/gothonweb/bin/app.py
@@ -1,12 +1,11 @@
 import web
-urls = (
-    '/','index'
-    )
+urls = ('/','index')
 app = web.application(urls,globals())
+render = web.template.render('templates/')
 class index(object):
     def GET(self):
         greeting = "Hello World"
-        return greeting
+        return render.index(greeting=greeting)
 
 if __name__ == "__main__":
     app.run()
